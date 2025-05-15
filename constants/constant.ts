959be7122f92bbdba491995f98889653479a7868
sushant81074 <sushant94601@gmail.com>
2025-05-15 15:34:35 +0000

    MIN BET TO 25

--- constants/constant.ts
+++ constants/constant.ts
@@ -2,7 +2,7 @@ import type { IGameSettings } from "../interfaces";
 
 export const GAME_SETTINGS: IGameSettings = {
     max_amt: 200000,
-    min_amt: 100,
+    min_amt: 25,
     max_co: 1000000,
     win_mult: 1.98,
     tie_mult: 1,
